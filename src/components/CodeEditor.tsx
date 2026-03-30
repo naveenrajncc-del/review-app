1796b3331c66c9f29c1af5702232edd3f0e6db11
Naveen Kumar R <naveenku,ar.r1@tesco.com>
2026-03-30 16:14:36 +0530

    test commit

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -8,6 +8,8 @@ const CodeEditor = ({ onSubmit }: Props) => {
   const [code, setCode] = useState('');
 console.log('CodeEditor rendered with code: ', code);
 console.log('CodeEditor rendered with code: ', code);
+console.log('CodeEditor rendered with code: ', code);
+
 
   return (
     <div>
