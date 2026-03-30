873ae5c6fab4aefb4f3880eb5b7ade58e6fc9551
Naveen Kumar R <naveenku,ar.r1@tesco.com>
2026-03-30 16:21:48 +0530

    test commit

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -11,7 +11,6 @@ console.log('CodeEditor rendered with code: ', code);
 console.log('CodeEditor rendered with code: ', code);
 console.log('CodeEditor rendered with code: ', code);
 
-console.log('CodeEditor rendered with code: ', code);
 
 
 
